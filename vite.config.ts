7eb8d4ecf8b80f9588d5372b44566a55c3703c19
asdf2501 <asdf_2501@hotmail.com>
2023-02-03 11:09:42 +0900

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Vue-Router-Sample',
+  base: '/Vue-Router-Sample/',
   plugins: [vue()],
   resolve: {
     alias: {
